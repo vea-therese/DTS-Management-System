37d218e94c8d581233d2af204800f8a35fd0ae96
Vea Therese <58013600+vea-therese@users.noreply.github.com>
2023-09-15 10:53:08 +0800

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,14 @@
+fa_custom_setup_kit('https://kit.fontawesome.com/c3481268b6.js');
+
+if (! function_exists('fa_custom_setup_kit') ) {
+  function fa_custom_setup_kit($kit_url = '') {
+    foreach ( [ 'wp_enqueue_scripts', 'admin_enqueue_scripts', 'login_enqueue_scripts' ] as $action ) {
+      add_action(
+        $action,
+        function () use ( $kit_url ) {
+          wp_enqueue_script( 'font-awesome-kit', $kit_url, [], null );
+        }
+      );
+    }
+  }
+}
